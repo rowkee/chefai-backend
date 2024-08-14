b9c05995c472a3d45e6fe6c26a2dda6036241917
rowkee <rick.keenan10@gmail.com>
2024-08-14 12:29:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import mealsFunction from "./config/openaiConfig.js";
+
+import express from "express";
+import cors from "cors";
+
+const app = express();
+const port = 4000;
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+app.use(express.json());
+app.use(express.static("public"));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.post("/meals", mealsFunction);
---
+++ config/openaiConfig.js
@@ -0,0 +1,50 @@
+import OpenAI from "openai";
+import "dotenv/config";
+
+const openai = new OpenAI({ apiKey: process.env.OPEN_AI_API_KEY });
+
+async function mealsFunction(req, res) {
+  const { input } = req.body;
+
+  const meals = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: `Create a list of simple meals, in UK format based on this: ${input}`,
+      },
+    ],
+    model: "gpt-4o-mini",
+    max_tokens: 500,
+  });
+
+  const mealsResponse = meals.choices[0].message.content;
+
+  console.log("Meals response:", mealsResponse);
+
+  const ingredientsList = await shoppingList(mealsResponse);
+
+  console.log("Ingredients List:", ingredientsList);
+
+  res.status(200).json({
+    meals: mealsResponse,
+    ingredients: ingredientsList,
+  });
+}
+
+async function shoppingList(meals) {
+  console.log(meals);
+
+  const ingredients = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: `create a shopping list based on in a csv format ${meals}`,
+      },
+    ],
+    model: "gpt-4o-mini",
+    max_tokens: 100,
+  });
+  return ingredients.choices[0].message.content;
+}
+
+export default mealsFunction;
