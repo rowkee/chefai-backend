2ffb094d3da00665df27d089751ff3e03de25ebd
rowkee <rick.keenan10@gmail.com>
2024-08-14 13:09:50 +0100

    this surely cannot fix it

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import mealsFunction from "./config/openaiConfig.js";
 import dotenv from "dotenv";
+dotenv.config();
+
+import mealsFunction from "./config/openaiConfig.js";
 
 import express from "express";
 import cors from "cors";
-
-dotenv.config();
 
 const app = express();
 const port = 4000;
